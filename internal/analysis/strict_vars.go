80a0e2a57c3dc0833390cbe90b4c6c37e7436ff9
Shoichi Kaji <skaji@cpan.org>
2026-01-22 00:44:35 +0900

    Allow hash slice deref via @

--- internal/analysis/strict_vars.go
+++ internal/analysis/strict_vars.go
@@ -359,12 +359,18 @@ func isSigilDeref(tokens []ppi.Token, idx int) bool {
 	if tok.Type == ppi.TokenSymbol && strings.HasPrefix(tok.Value, "$") {
 		return true
 	}
+	if tok.Type == ppi.TokenSymbol && strings.HasPrefix(tok.Value, "%") {
+		return true
+	}
 	if tok.Type == ppi.TokenOperator && tok.Value == "{" {
 		nextVar := nextNonTrivia(tokens, next+1)
 		if nextVar < 0 {
 			return false
 		}
 		if tokens[nextVar].Type == ppi.TokenSymbol && strings.HasPrefix(tokens[nextVar].Value, "$") {
+			return true
+		}
+		if tokens[nextVar].Type == ppi.TokenSymbol && strings.HasPrefix(tokens[nextVar].Value, "%") {
 			return true
 		}
 	}
